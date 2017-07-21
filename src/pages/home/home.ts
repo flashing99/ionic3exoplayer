e7641239ecb4253ef1b6c6d6b480c3b7fec5ba55
Antonio <antoniowyep@gmail.com>
2017-07-21 10:43:08 -0400

    Ionic with Exoplayer example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+declare var ExoPlayer;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public plt: Platform) {
   }
+	openLiveRadio() {
+	  var params = { 
+			url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/WYEPFMAAC.aac',
+			userAgent: 'MyAwesomePlayer', // default is 'ExoPlayerPlugin'
+			aspectRatio: 'FILL_SCREEN', // default is FIT_SCREEN
+			hideTimeout: 5000, // Hide controls after this many milliseconds, default is 5 sec
+			seekTo: 0, // Start playback 10 minutes into video specified in milliseconds, default is 0
+			skipTime: 0, // Amount of time to use when going forward/backward, default is 1 min
+			controller: { // If this object is not present controller will not be visible
+					hideProgress: false,
+					controlIcons: {
+							'exo_rew': 'http://url.to/rew.png',
+							'exo_play': 'http://url.to/play.png',
+							'exo_pause': 'http://url.to/pause.png',
+							'exo_ffwd': 'http://url.to/ffwd.png'
+					}
+			}
+		}
+		this.plt.ready().then(() => {
+			  ExoPlayer.show(params);
+    });
+  }
+
 
 }
